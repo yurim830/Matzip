bf3236c7ea3f5315852754e5d7832489084df66d
Yurim Kim <yurim830@gmail.com>
2025-10-08 00:30:42 +0900

    [Feat] Drawer - 프로필 영역 구현

--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -3,14 +3,33 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import React, {use} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Pressable,
+} from 'react-native';
 import {colors} from '../constants/colors';
 
 function CustomDrawerContent(props: DrawerContentComponentProps) {
   return (
     <SafeAreaView style={styles.container}>
-      <DrawerContentScrollView {...props}>
+      <DrawerContentScrollView
+        {...props}
+        scrollEnabled={false}
+        contentContainerStyle={styles.contentContainer}>
+        <Pressable style={styles.profileContainer}>
+          <View style={styles.userImageContainer}>
+            <Image
+              source={require('../assets/default-user.png')}
+              style={styles.userImage}
+            />
+          </View>
+          <Text style={styles.nickname}>닉네임</Text>
+        </Pressable>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
       <View style={styles.bottomContainer}>
@@ -24,6 +43,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  contentContainer: {
+    gap: 5,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+    gap: 5,
+  },
+  userImageContainer: {
+    height: 70,
+    width: 70,
+    borderRadius: 35,
+  },
+  userImage: {
+    height: '100%',
+    width: '100%',
+  },
+  nickname: {
+    fontSize: 14,
+  },
   bottomContainer: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
@@ -31,6 +70,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: colors.GRAY_200,
   },
+
   settingText: {
     fontSize: 16,
     color: colors.BLACK,
